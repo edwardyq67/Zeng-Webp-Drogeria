0da4642fc14cc40ea7b239282156934bda320726
edward672001 <edwardyq200167@gmail.com>
2026-01-23 13:53:32 -0500

    actualizado

--- src/app/api/reclamos/route.js
+++ src/app/api/reclamos/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Función para generar código único
 function generarCodigoReclamo() {
